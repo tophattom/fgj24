e7feedd45a61bfb2cfe7d313034360369e76fe67
Jaakko Rinta-Filppula <jaakko@r-f.fi>
2024-01-26 18:23:09 +0200

    Oops

--- source/Main.hx
+++ source/Main.hx
@@ -4,9 +4,8 @@ import flixel.FlxGame;
 import openfl.display.Sprite;
 
 class Main extends Sprite {
-	public function new()
-		addChild(new FlxGame(0, 0, PlayState));
-	{
+	public function new() {
 		super();
+		addChild(new FlxGame(0, 0, PlayState));
 	}
 }
